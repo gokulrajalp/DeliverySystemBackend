8b0b31876a641304ccc6dab317e34dd64cb36ab5
gokulrajalp <gokulrajalp@gmail.com>
2022-12-02 15:16:26 +0530

    Updated

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express=require('express');
 const connectToMongo = require('./config/mongoose')
 const app = express()
-const port=8000;
+// const port=8000;
+const port= process.env.PORT || 8000;
 const cors=require('cors');
 
 
